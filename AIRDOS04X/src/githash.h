75e8460b7cd2e3a8c07a65a5caff16d77914021e
Vít Hanousek <vithanousek@seznam.cz>
2026-06-10 13:56:59 +0200

    Remove Deprecated version AIRDOS04X.

--- AIRDOS04X/src/githash.h
+++
@@ -1,5 +0,0 @@
-// This file is overwritten by github actions, do not update it manually
-String githash = "35e771c738d8b9ccecdcbe6273fa81bced61ecb8,User";
-#define GHRELEASE 0
-#define GHBUILD 0
-#define GHBUILDTYPE User
