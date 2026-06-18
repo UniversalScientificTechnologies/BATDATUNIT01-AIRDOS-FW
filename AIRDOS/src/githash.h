5bff19104c8f61ace7b779fc173726d00be952a5
ustbot <ust-bot@seznam.cz>
2026-06-18 21:27:41 +0000

    FIX: Start from 0 release, not -1, extended build

--- AIRDOS/src/githash.h
+++ AIRDOS/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "4b226b5e6ea76e9356c3c1b77c65950d3f9f7702,User";
+String githash = "28a1f8a1d3eed458ca15ac87fd0ac666d42ef0c6,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
