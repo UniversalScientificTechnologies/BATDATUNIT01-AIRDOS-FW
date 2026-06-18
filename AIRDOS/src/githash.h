d8c5b9e62138891b8d7a75d2453143d7fe3f23b9
ustbot <ust-bot@seznam.cz>
2026-06-18 21:55:38 +0000

    Merge remote-tracking branch 'origin/main', extended build, extended build

--- AIRDOS/src/githash.h
+++ AIRDOS/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "60fb1888bb9fd8020e3482d8deddf2c3f5baf1d8,User";
+String githash = "d985dde60c2bb87eeb461f6ffa48f4b94bd40337,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
