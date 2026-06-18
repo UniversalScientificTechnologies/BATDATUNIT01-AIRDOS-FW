14419dbc59177a3971c4b14ccab045f431e2ccd8
ustbot <ust-bot@seznam.cz>
2026-06-18 21:34:22 +0000

    Test commit, extended build

--- AIRDOS/src/githash.h
+++ AIRDOS/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "5bff19104c8f61ace7b779fc173726d00be952a5,User";
+String githash = "3dcd012af68b9e201938c5cfa26817e56af055fc,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
