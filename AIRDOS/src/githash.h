a876132ea0f1c48245eef1164e14a93faa3959be
ustbot <ust-bot@seznam.cz>
2026-06-18 21:36:18 +0000

    Fix name in release, extended build

--- AIRDOS/src/githash.h
+++ AIRDOS/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "3dcd012af68b9e201938c5cfa26817e56af055fc,User";
+String githash = "2e5d4002b21324085234e9721f9d443a09840430,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
