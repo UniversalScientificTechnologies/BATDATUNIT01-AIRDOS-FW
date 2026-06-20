a32d4caf65e9ebca29b94421fda6ab83b9e5c167
ustbot <ust-bot@seznam.cz>
2026-06-20 23:35:12 +0000

    Merge pull request #3 from UniversalScientificTechnologies/fix/button-poweroff-skip-c21-discharge, extended build, extended build

--- AIRDOS/src/githash.h
+++ AIRDOS/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "4dad254a1d59ecab4a9a0c7aa4c45f8f29163a05,User";
+String githash = "748f2fd5842ab2e9e5135ce3dd3d8053795dd066,User";
 #define GHRELEASE 2
 #define GHBUILD 0
 #define GHBUILDTYPE User
